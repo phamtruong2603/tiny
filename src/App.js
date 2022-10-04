d8a074b29dba5970861981cb551be60f01c9f287
phamtruong2603 <pxtruong2603@gmail.com>
2022-10-05 00:53:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,122 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
-function App() {
-
-  const [contents, setContents] = useState([]);
-
-  const editorRef = useRef();
-
+export default function App() {
+  const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
-      setContents([
-        {
-          title: '',
-          content: editorRef.current.getContent()
-        },
-        ...contents
-      ])
     }
   };
-
+  const youtube_parser = (url) => {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match && match[7].length === 11) ? match[7] : false;
+  }
+  
   return (
-    <div className="App">
+    <>
       <Editor
         onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>Tiny MCE editor.</p>"
-        // apiKey='your-api-key'
-        // inline={false}
-        // outputFormat='text' // định dạng đầu ra
+        initialValue=''
+        apiKey='qncahgo1xvpwopoou5vjkysx5glaxgrkxa7o71jn9tqu1d31'
         init={{
-          height: 350,
+          height: 500,
           menubar: true,
-          plugins: [
-            // 'advlist autolink lists link image charmap print preview anchor',
-            // 'searchreplace visualblocks code fullscreen',
-            // 'insertdatetime media table paste code help wordcount',
+          file_picker_callback: (callback, value, meta) => {
+            //Upload files from computer
+            if (meta.filetype === 'image') {
+              const input = document.createElement('input');
+              input.setAttribute('type', 'file');
+              input.setAttribute('accept', 'image/*');
+              input.addEventListener('change', (e) => {
+                console.log(e)
+                const file = e.target.files[0];
+                const reader = new FileReader();
+                reader.addEventListener('load', () => {
+                  const id = 'blobid' + (new Date()).getTime();
+                  const blobCache = window.tinymce.activeEditor.editorUpload.blobCache;
+                  const base64 = reader.result.split(',')[1];
+                  const blobInfo = blobCache.create(id, file, base64);
+                  blobCache.add(blobInfo);
+                  callback(blobInfo.blobUri(), { title: file.name });
+                });
+                reader.readAsDataURL(file);
+              });
+              input.click();
+            }
+          },
+          setup: editor => {
+            editor.editorManager.PluginManager.add("YouTB", function (
+              editor
+            ) {
+              const openDialog = () => editor.windowManager.open({
+                title: 'Link',
+                body: {
+                  type: 'panel',
+                  items: [
+                    {
+                      type: 'input',
+                      name: 'title',
+                      label: 'your path'
+                    }
+                  ]
+                },
+                buttons: [
+                  {
+                    type: 'cancel',
+                    text: 'Close'
+                  },
+                  {
+                    type: 'submit',
+                    text: 'Save',
+                    buttonType: 'primary'
+                  }
+                ],
+                onSubmit: (api) => {
+                  const data = api.getData();
+                  const url = youtube_parser(data.title)
+                  editor.setContent(
+                    `<iframe src="https://youtube.com/embed/${url}"></iframe>`,
+                    { format: 'html' });
+                  console.log(data.title)
+                  api.close();
+                }
+              });
+              editor.ui.registry.addButton('YouTB', {
+                text: 'YouTB',
+                onAction: () => {
+                  openDialog();
+                }
+              });
+              editor.ui.registry.addMenuItem('YouTB', {
+                text: 'YouTB plugin',
+                onAction: () => {
+                  openDialog();
+                }
+              });
+              return {
+                getMetadata: () => ({
+                  name: 'YouTB plugin',
+                  url: ''
+                })
+              };
+            })
+          },
 
-            'advlist', 'autolink', 'lists', 'link', 'image', 'table',
-            'charmap', 'preview', 'anchor', 'pagebreak', 'tinymcespellchecker'
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount', 'YouTB'
           ],
           toolbar: 'undo redo | formatselect | ' +
-            'bold italic backcolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent |  ' +
+            'YouTB',
+          content_style: 'body {font - family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
       />
       <button onClick={log}>Log editor content</button>
-      <div className='body'>
-        {contents.map((content) => {
-          const text = content.content
-          return (
-            <div dangerouslySetInnerHTML={{ __html: text }} />
-          )
-        })}
-      </div>
-    </div>
+    </>
   );
 }
-
-export default App;
